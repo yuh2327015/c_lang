12e8b66caa8aca4cf307f7338854c5e1c0464b75
yuh2327015 <www9216@naver.com>
2023-05-09 12:59:17 +0900

    과제, 하는중

--- lecture10/gu.c
+++ lecture10/gu.c
@@ -2,28 +2,25 @@
 
 int print_title_screen()
 {
-	printf("###############################\n");
-	printf("##                           ##\n");
-	printf("##         Dino Run          ##\n");
-	printf("##          V.0.1            ##\n");
-	printf("##                           ##\n");
-	printf("##  1. Start Game            ##\n");
-	printf("##  2. How to Play           ##\n");
-	printf("##  3. Exit                  ##\n");
-	printf("##                           ##\n");
-	printf("###############################\n");
+	printf("\nDino Run\n");
+	printf("V.0.1\n");
+	printf("\n");
+	printf("1. Start Game\n");
+	printf("2. How to Play\n");
+	printf("3. Exit\n");
 
 	return 0;
 }
 
-int print_how_to_play_screen()
+int how_to_play()
 {
 	printf("###############################\n");
 	printf("##                           ##\n");
 	printf("##      To Avoid Enemy       ##\n");
 	printf("##                           ##\n");
 	printf("##  Press Space Bar To Jump  ##\n");
-	printf("##                           ##\n");
+	printf("##  1.go title               ##\n");
+	printf("##  2.stay here              ##\n");
 	printf("###############################\n");
 
 	return 0;
@@ -33,23 +30,50 @@ int main()
 {
 	int game_state = 1;
 	
-	char map[465]; /* 가로 30 + 1(개행문자) 세로 15 */
+	char map[33]; /* 가로 30 + 1(개행문자) 세로 15 */
 
 	int i = 0;
-	while(i<465)
+	while(i<32)
 	{
 		map[i]='0';
 		i=i+1; //i++
 	}
 
 	i = 0;
-	while(i<465)
+	while(i<32)
 	{
 		printf("%c", map[i]);
 		i = i+1;
-		if (i % 31 == 0)
+		if (i % 17== 0)
 		{
-			printf("\n");
+			print_title_screen();
+			int input = 0;
+	while(game_state)
+	{
+		scanf("%d",&input);
+		if(input==3)
+		{
+			game_state=0;
+		}
+		if(input==2)
+		{
+			how_to_play();
+		}
+		if(input==1)
+		{
+        printf("##################################\n");
+	printf("##				##\n");
+	printf("##	     Dino Run		##\n");
+	printf("##	     - V0.1 -		##\n");
+	printf("##				##\n");
+	printf("##  1. Game Start		##\n");
+	printf("##  2. How To Play		##\n");
+	printf("##  3. Exit 			##\n");
+	printf("##		      		##\n");
+	printf("##################################\n");
+ 	printf("input(1~3)");
+		}
+	}
 		}
 	}
 	return 0;
